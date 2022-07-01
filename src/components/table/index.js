75ee577e3f96a28cc0dd8db10d44951001daadaf
JasurbekYuldashov <jasurbekyuldashov123@gmail.com>
2022-07-02 00:13:13 +0500

    push

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
+import {alpha} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -20,33 +20,10 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
 // import DeleteIcon from '@mui/icons-material/Delete';
 // import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
+import {visuallyHidden} from '@mui/utils';
+import {AiFillDelete} from "react-icons/ai";
+import moment from "moment";
 
-function createData(name, calories, fat, carbs, protein) {
-    return {
-        name,
-        calories,
-        fat,
-        carbs,
-        protein,
-    };
-}
-
-const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
-];
 
 function descendingComparator(a, b, orderBy) {
     if (b[orderBy] < a[orderBy]) {
@@ -64,8 +41,6 @@ function getComparator(order, orderBy) {
         : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
 function stableSort(array, comparator) {
     const stabilizedThis = array.map((el, index) => [el, index]);
     stabilizedThis.sort((a, b) => {
@@ -78,41 +53,8 @@ function stableSort(array, comparator) {
     return stabilizedThis.map((el) => el[0]);
 }
 
-const headCells = [
-    {
-        id: 'name',
-        numeric: false,
-        disablePadding: true,
-        label: 'Dessert (100g serving)',
-    },
-    {
-        id: 'calories',
-        numeric: true,
-        disablePadding: false,
-        label: 'Calories',
-    },
-    {
-        id: 'fat',
-        numeric: true,
-        disablePadding: false,
-        label: 'Fat (g)',
-    },
-    {
-        id: 'carbs',
-        numeric: true,
-        disablePadding: false,
-        label: 'Carbs (g)',
-    },
-    {
-        id: 'protein',
-        numeric: true,
-        disablePadding: false,
-        label: 'Protein (g)',
-    },
-];
-
 function EnhancedTableHead(props) {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+    const {onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort, headCells} =
         props;
     const createSortHandler = (property) => (event) => {
         onRequestSort(event, property);
@@ -168,13 +110,13 @@ EnhancedTableHead.propTypes = {
 };
 
 const EnhancedTableToolbar = (props) => {
-    const { numSelected } = props;
+    const {numSelected} = props;
 
     return (
         <Toolbar
             sx={{
-                pl: { sm: 2 },
-                pr: { xs: 1, sm: 1 },
+                pl: {sm: 2},
+                pr: {xs: 1, sm: 1},
                 ...(numSelected > 0 && {
                     bgcolor: (theme) =>
                         alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
@@ -183,7 +125,7 @@ const EnhancedTableToolbar = (props) => {
         >
             {numSelected > 0 ? (
                 <Typography
-                    sx={{ flex: '1 1 100%' }}
+                    sx={{flex: '1 1 100%'}}
                     color="inherit"
                     variant="subtitle1"
                     component="div"
@@ -192,7 +134,7 @@ const EnhancedTableToolbar = (props) => {
                 </Typography>
             ) : (
                 <Typography
-                    sx={{ flex: '1 1 100%' }}
+                    sx={{flex: '1 1 100%'}}
                     variant="h6"
                     id="tableTitle"
                     component="div"
@@ -204,13 +146,13 @@ const EnhancedTableToolbar = (props) => {
             {numSelected > 0 ? (
                 <Tooltip title="Delete">
                     <IconButton>
-                        <DeleteIcon />
+                        <AiFillDelete/>
                     </IconButton>
                 </Tooltip>
             ) : (
                 <Tooltip title="Filter list">
                     <IconButton>
-                        <FilterListIcon />
+                        {/*<FilterListIcon />*/}
                     </IconButton>
                 </Tooltip>
             )}
@@ -222,7 +164,7 @@ EnhancedTableToolbar.propTypes = {
     numSelected: PropTypes.number.isRequired,
 };
 
-export default function MuiTable() {
+export default function MuiTable({headCells, rows}) {
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('calories');
     const [selected, setSelected] = React.useState([]);
@@ -285,12 +227,12 @@ export default function MuiTable() {
         page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Paper sx={{ width: '100%', mb: 2 }}>
-                <EnhancedTableToolbar numSelected={selected.length} />
+        <Box sx={{width: '100%'}}>
+            <Paper sx={{width: '100%', mb: 2}}>
+                <EnhancedTableToolbar numSelected={selected.length}/>
                 <TableContainer>
                     <Table
-                        sx={{ minWidth: 750 }}
+                        sx={{minWidth: 750}}
                         aria-labelledby="tableTitle"
                         size={dense ? 'small' : 'medium'}
                     >
@@ -301,6 +243,7 @@ export default function MuiTable() {
                             onSelectAllClick={handleSelectAllClick}
                             onRequestSort={handleRequestSort}
                             rowCount={rows.length}
+                            headCells={headCells}
                         />
                         <TableBody>
                             {/* if you don't need to support IE11, you can replace the `stableSort` call with:
@@ -330,18 +273,44 @@ export default function MuiTable() {
                                                     }}
                                                 />
                                             </TableCell>
-                                            <TableCell
-                                                component="th"
-                                                id={labelId}
-                                                scope="row"
-                                                padding="none"
-                                            >
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell align="right">{row.calories}</TableCell>
-                                            <TableCell align="right">{row.fat}</TableCell>
-                                            <TableCell align="right">{row.carbs}</TableCell>
-                                            <TableCell align="right">{row.protein}</TableCell>
+                                            {
+                                                headCells?.map((r, index) => {
+                                                    if (r.id === "edit") {
+                                                        return (
+                                                            <TableCell
+                                                                align="right">{typeof row.edit === "function" && row.edit(row)}</TableCell>
+                                                        )
+                                                    }
+                                                    if (index === 0) {
+                                                        return (
+                                                            <TableCell
+                                                                component="th"
+                                                                id={labelId}
+                                                                scope="row"
+                                                                padding="none"
+                                                            >
+                                                                {!r.isTime ? row[r.id] : moment(row[r.id]).format("Do:MMM:YYYY HH:mm")}
+                                                            </TableCell>
+                                                        )
+                                                    }
+                                                    return (
+                                                        <TableCell
+                                                            align="right">{!r.isTime ? row[r.id] : moment(row[r.id]).format("Do,MMM YYYY HH:mm")}</TableCell>
+                                                    )
+                                                })
+                                            }
+                                            {/*<TableCell*/}
+                                            {/*    component="th"*/}
+                                            {/*    id={labelId}*/}
+                                            {/*    scope="row"*/}
+                                            {/*    padding="none"*/}
+                                            {/*>*/}
+                                            {/*    {row.email}*/}
+                                            {/*</TableCell>*/}
+                                            {/*<TableCell align="right">{row.calories}</TableCell>*/}
+                                            {/*<TableCell align="right">{row.fat}</TableCell>*/}
+                                            {/*<TableCell align="right">{row.carbs}</TableCell>*/}
+                                            {/*<TableCell align="right">{row.protein}</TableCell>*/}
                                         </TableRow>
                                     );
                                 })}
@@ -351,7 +320,7 @@ export default function MuiTable() {
                                         height: (dense ? 33 : 53) * emptyRows,
                                     }}
                                 >
-                                    <TableCell colSpan={6} />
+                                    <TableCell colSpan={6}/>
                                 </TableRow>
                             )}
                         </TableBody>
@@ -365,10 +334,11 @@ export default function MuiTable() {
                     page={page}
                     onPageChange={handleChangePage}
                     onRowsPerPageChange={handleChangeRowsPerPage}
+
                 />
             </Paper>
             <FormControlLabel
-                control={<Switch checked={dense} onChange={handleChangeDense} />}
+                control={<Switch checked={dense} onChange={handleChangeDense}/>}
                 label="Dense padding"
             />
         </Box>
--- src/containers/adminUsers/index.js
+++ src/containers/adminUsers/index.js
@@ -8,9 +8,10 @@ import {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {getAllUser} from "../../redux/user/actions";
 import MuiTable from "../../components/table";
+import TableCell from '@mui/material/TableCell';
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+    const {children, value, index, ...other} = props;
 
     return (
         <div
@@ -21,7 +22,7 @@ function TabPanel(props) {
             {...other}
         >
             {value === index && (
-                <Box sx={{ p: 3 }}>
+                <Box sx={{p: 3}}>
                     <Typography>{children}</Typography>
                 </Box>
             )}
@@ -42,6 +43,59 @@ function a11yProps(index) {
     };
 }
 
+
+const headCells = [
+    {
+        id: 'email',
+        numeric: false,
+        disablePadding: true,
+        label: 'Email',
+        isTime: false
+    },
+    {
+        id: 'fullName',
+        numeric: true,
+        disablePadding: false,
+        label: 'Fullname',
+        isTime: false
+    },
+    {
+        id: 'city',
+        numeric: true,
+        disablePadding: false,
+        label: 'City',
+        isTime: false
+    },
+    {
+        id: 'phone',
+        numeric: true,
+        disablePadding: false,
+        label: 'Phone',
+        isTime: false
+    },
+    {
+        id: 'nameCompany',
+        numeric: true,
+        disablePadding: false,
+        label: 'Company name',
+        isTime: false
+    },
+    {
+        id: 'createdAt',
+        numeric: true,
+        disablePadding: false,
+        label: 'Created time',
+        isTime: true
+    },
+    {
+        id: 'edit',
+        numeric: true,
+        disablePadding: false,
+        label: 'Actions',
+        isTime: false
+    },
+];
+
 export default function AdminUsers() {
     const dispatch = useDispatch();
     const [value, setValue] = React.useState(0);
@@ -50,15 +104,15 @@ export default function AdminUsers() {
         setValue(newValue);
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(getAllUser())
-    },[])
+    }, [])
 
     const users = useSelector(state => state.user.users)
     console.log(users)
     return (
-        <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Box sx={{width: '100%'}}>
+            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
                     <Tab label="Admins" {...a11yProps(0)} />
                     <Tab label="Agents" {...a11yProps(1)} />
@@ -66,13 +120,26 @@ export default function AdminUsers() {
                 </Tabs>
             </Box>
             <TabPanel value={value} index={0}>
-                <MuiTable/>
+                <MuiTable
+                    rows={users.admin.map(r => {
+                        return {
+                            ...r,
+                            edit:(item)=> <div>{JSON.stringify(item)}</div>
+                        }
+                    })}
+                    headCells={headCells}
+                />
             </TabPanel>
             <TabPanel value={value} index={1}>
-                <MuiTable/>
+                <MuiTable rows={users.agent} headCells={headCells}/>
             </TabPanel>
             <TabPanel value={value} index={2}>
-                <MuiTable/>
+                <MuiTable rows={users.newAgent.map(r => {
+                    return {
+                        ...r,
+                        edit: (item)=> <div>{JSON.stringify(item)}</div>
+                    }
+                })} headCells={headCells}/>
             </TabPanel>
         </Box>
     );
