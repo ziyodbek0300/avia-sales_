cd2f095a7d2ce4a574a8626de17999f247bd12df
JasurbekYuldashov <jasurbekyuldashov123@gmail.com>
2022-07-29 10:09:30 +0500

    push

--- sales-backend/routes/index.js
+++ sales-backend/routes/index.js
@@ -21,52 +21,25 @@ router.post('/', function (req, res, next) {
         })
 });
 
-router.post('/', function (req, res, next) {
-    Promise.all([axios.get("http://smartsys.intouch.ae/incoming/export/default.php?samo_action=reference&form=http://samo.travel&type=currentstamp")])
+router.post('/getHotels/:townId', function (req, res, next) {
+    Promise.all([axios.post(`http://smartsys.intouch.ae/incoming/export/default.php?samo_action=auth`, {
+        login: "ContinenTashtXML",
+        passwordDigest: "DfuUuLNhnl13Uu%2FSkJuQYZ9%2B9cr6vmlZ3W9vjPAywgRY7c9elyMp9GHRmwsN%2FPKzCyhacrhHM9Po2JLV1gHgRQ%3D%3D"
+    }, {headers: {"Content-type": "application/x-www-form-urlencoded"}})])
         .then(r => {
-            parseString(r[0].data, function (err, result) {
-                res.send(result)
-            });
+            console.log(r[0].data)
+            const token = ""
+            Promise.all([axios.get(`http://smartsys.intouch.ae/incoming/export/default.php?samo_action=reference&partner_token=${token}&form=http://samo.travel&type=hotel&town=${req.params.townId}`)])
+                .then(r => {
+                    parseString(r[0].data, function (err, result) {
+                        res.send(result)
+                    });
+                })
+                .catch(e => {
+                    res.status(404).send(ErrorSend(404, e, e.message))
+                })
         })
-        .catch(e => {
-            res.status(404).send(ErrorSend(404, e, e.message))
-        })
-});
-
-router.post('/', function (req, res, next) {
-    Promise.all([axios.get("http://smartsys.intouch.ae/incoming/export/default.php?samo_action=reference&form=http://samo.travel&type=currentstamp")])
-        .then(r => {
-            parseString(r[0].data, function (err, result) {
-                res.send(result)
-            });
-        })
-        .catch(e => {
-            res.status(404).send(ErrorSend(404, e, e.message))
-        })
-});
-
-router.post('/', function (req, res, next) {
-    Promise.all([axios.get("http://smartsys.intouch.ae/incoming/export/default.php?samo_action=reference&form=http://samo.travel&type=currentstamp")])
-        .then(r => {
-            parseString(r[0].data, function (err, result) {
-                res.send(result)
-            });
-        })
-        .catch(r => {
-            res.status(404).send(ErrorSend(404, e, e.message))
-        })
-});
-
-router.post('/', function (req, res, next) {
-    Promise.all([axios.get("http://smartsys.intouch.ae/incoming/export/default.php?samo_action=reference&form=http://samo.travel&type=currentstamp")])
-        .then(r => {
-            parseString(r[0].data, function (err, result) {
-                res.send(result)
-            });
-        })
-        .catch(r => {
-            res.status(404).send(ErrorSend(404, e, e.message))
-        })
+        .catch()
 });
 
 module.exports = router;
