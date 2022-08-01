16afa4f3f2ec5ca8219e8cd06a51752f018ea03f
JasurbekYuldashov <jasurbekyuldashov123@gmail.com>
2022-08-01 10:18:13 +0500

    push

--- sales-backend/controller/ticket.js
+++ sales-backend/controller/ticket.js
@@ -1,9 +1,28 @@
 const model = require("../models");
 const {Success, ErrorSend} = require("../service/SuccessAndError");
+const userRole = require("../constants/userRole");
 
-const getAll = (req,res,next)=>{
-    // model.
-    next()
+const getAll = async  (req,res,next)=>{
+    try {
+        // console.log(model.Ticket)
+        if (!req.user || req.user.role === userRole.client || req.user.role === userRole.agent) {
+            if (req.user && req.user.role === userRole.agent) {
+                return res.status(404).send(ErrorSend(404, {}, "no user"))
+            } else {
+                return res.status(404).send(ErrorSend(404, {}, "no user"))
+            }
+        }
+        Promise.all([model.Ticket.findAll({})])
+            .then(r => {
+
+                return res.status(200).send(Success(200, r[0], "ok"))
+            })
+            .catch(e => {
+                return res.status(404).send({code: 404, error: e, message: e.message})
+            })
+    } catch (e) {
+        return res.status(505).send({code: 505, error: e, message: e.message})
+    }
 }
 
 module.exports = {
