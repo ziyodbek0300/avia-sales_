fedee6feb8513e1dd905c51300cf5e34ff3c145f
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-09-01 17:44:38 +0500

    api changed to global

--- src/api/projectApi/index.js
+++ src/api/projectApi/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import Cookies from 'js-cookie';
 
-// export const MainApi = "http://travelcontinent.uz/api";
+export const MainApi = "http://travelcontinent.uz/api";
 // export const MainApi = "http://localhost";
-export const MainApi = "http://192.168.31.74";
+// export const MainApi = "http://192.168.31.74";
 
 const instance = axios.create({
     baseURL: `${MainApi}/`,
