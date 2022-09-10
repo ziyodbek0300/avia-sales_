f58140b7abee349836a6ed4cb97acd5f68187720
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-09-10 17:40:09 +0500

    some

--- src/containers/home/TabSections/Hotels.jsx
+++ src/containers/home/TabSections/Hotels.jsx
@@ -101,7 +101,8 @@ const RenderItem = ({e}) => {
                                                                     <input name={"asd"}
                                                                            className={"absolute hidden h-full w-full top-0 left-0"}
                                                                            type="radio"/>
-                                                                    <div className={"flex flex-col justify-between h-full"}>
+                                                                    <div
+                                                                        className={"flex flex-col justify-between h-full"}>
                                                                         <div>
                                                                             <h3>{e.name ? e.name : "Standart"}</h3>
                                                                             <p className={"text-xl"}>Price:
@@ -109,8 +110,9 @@ const RenderItem = ({e}) => {
                                                                             <p>{e.dataa.name}</p>
                                                                         </div>
                                                                         <div className={"text-right"}>
-                                                                            <button className={"px-4 py-2 bg-white text-zinc-900 font-bold capitalize rounded"}
-                                                                                    onClick={() => navigate(`hotel/order/${hotelId}/${e.inc}?name=${e.name}&adult=${1}&c=${2}&d=${3}`)}>
+                                                                            <button
+                                                                                className={"px-4 py-2 bg-white text-zinc-900 font-bold capitalize rounded"}
+                                                                                onClick={() => navigate(`hotel/order/${hotelId}/${e.inc}?name=${e.name}&adult=${1}&c=${2}&d=${3}`)}>
                                                                                 order
                                                                             </button>
                                                                         </div>
@@ -157,6 +159,7 @@ function Hotels() {
         datebeg: null,
         dateend: null
     })
+    const [search, setSearch] = useState("")
     const [adults, setAdults] = useState(1);
     const [infant, setInfant] = useState(0);
     const [children, setChildren] = useState(0);
@@ -310,14 +313,25 @@ function Hotels() {
                 </div>
             </div>
             <div className="max-w-5xl mx-auto flex flex-col gap-3">
+                {hotels.length !== 0 ? (<div className={"flex justify-end"}><input placeholder={"Search"}
+                                                                                   className={"border border-red-500 p-2 rounded"}
+                                                                                   type={"search"}
+                                                                                   onInput={(e) => setSearch(e.target.value)}/>
+                    </div>)
+                    : ("")}
                 {hotels.map(e => {
                     if (!(e.status !== 'D' && e.name !== "" && e.name?.toLowerCase() !== "unknown hotel" && e.name !== undefined)) {
                         return null;
                     }
-                    console.log(e.dataa)
-                    return (
-                        <RenderItem e={e}/>
-                    )
+                    try {
+                        return e.name.toLowerCase().includes(search.toLowerCase()) ? (
+                            <RenderItem e={e}/>
+                        ) : (<div className={"text-center"}>
+                            <h1>No Compared Data</h1>
+                        </div>)
+                    } catch (e) {
+                        console.log(e)
+                    }
                 })}
             </div>
         </>
