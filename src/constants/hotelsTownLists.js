86c850b0168a213f6448cc8cc3bcef813cdf2dc0
JasurbekYuldashov <jasurbekyuldashov123@gmail.com>
2022-09-15 00:38:15 +0500

    push

--- src/constants/hotelsTownLists.js
+++ src/constants/hotelsTownLists.js
@@ -2,12 +2,13 @@ const hotelsTownLists = [
     {id: 59595959, title: "Tashkent", ltitle: "Tashkent"},
     {id: 5, title: "Dubai", ltitle: "Dubai"},
     {id: 37, title: "Abu Dhabi", ltitle: "Abu Dhabi"},
-    {id: 9, title: "Sharjah", ltitle: "Sharjah"},
-    {id: 10, title: "Ajman", ltitle: "Ajman"},
-    {id: 30, title: "Al Ain", ltitle: "Al Ain"},
-    {id: 11, title: "Ras Al Khaimah", ltitle: "Ras Al Khaimah"},
-    {id: 12, title: "Fujairah", ltitle: "Fujairah"},
-    {id: 13, title: "Umm Al Quwain", ltitle: "Umm Al Quwain"},
+    {id: 36, title: "Sharjah", ltitle: "Sharjah"},
+    {id: 32, title: "Ajman", ltitle: "Ajman"},
+    {id: 104, title: "Al Ain", ltitle: "Al Ain"},
+    {id: 35, title: "Ras Al Khaimah", ltitle: "Ras Al Khaimah"},
+    {id: 33, title: "Fujairah", ltitle: "Fujairah"},
+    {id: 70, title: "Umm Al Quwain", ltitle: "Umm Al Quwain"},
 ]
+
 
 export default hotelsTownLists
