c1ad8e55b9fad554e9421c74eb24026d306cea09
jjkyuldashov <99941470+jjkyuldashov@users.noreply.github.com>
2022-11-11 01:06:41 +0500

    push

--- sales-backend/routes/registration.js
+++ sales-backend/routes/registration.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {registrationUser, loginUser} = require("../controller/registration");
+const {registrationUser, loginUser, attach} = require("../controller/registration");
 const router = express.Router();
 const multer = require('multer')
 const upload = multer({dest: './public/pdf/'})
 
 router.post('/', upload.single('doc'), registrationUser);
 router.post('/login', loginUser);
+router.post('/attach',upload.array('file'), attach);
 
 module.exports = router;
